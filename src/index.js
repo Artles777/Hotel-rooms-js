eafcc1fdeb009679fc6ec1cf6c107ea91e7644ea
Artem Alesenko <62352758+Artles777@users.noreply.github.com>
2020-10-25 17:54:41 +0300

    Completed config webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles/style.scss"
+import fonts from "google-fonts"
+
+fonts.add({
+    'Montserrat': ['400', '700'],
+    'Quicksand': ['300', '400', '700']
+})
---
+++ webpack.config.js
@@ -0,0 +1,163 @@
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const {merge} = require('webpack-merge')
+const MediaQueryPlugin = require('media-query-plugin')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+const isProd = process.env.NODE_ENV == 'production'
+const isDev = !isProd
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+
+const jsLoaders = () => {
+    const loaders = [
+        {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            },
+        },
+    ]
+    if (isDev) {
+        loaders.push('eslint-loader')
+    }
+    return loaders
+}
+
+console.log('prod', isProd)
+console.log('dev', isDev)
+
+module.exports = merge([
+    {
+        context: path.resolve(__dirname, 'src'),
+        entry: ['@babel/polyfill', './index.js'],
+        output: {
+            filename: filename('js'),
+            path: path.resolve(__dirname, 'dist')
+        },
+        resolve: {
+            extensions: ['.js'],
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@core': path.resolve(__dirname, 'src/core'),
+            }
+        },
+        optimization: {
+            minimizer: [new OptimizeCSSAssetsPlugin({})],
+            runtimeChunk: 'single',
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors',
+                        chunks: 'all',
+                    },
+                },
+            },
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            index: 'index.html',
+            hot: isDev,
+            compress: isDev,
+            open: true,
+            port: 4000
+        },
+        devtool: isDev ? 'source-map' : false,
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        {
+                            loader: isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {importLoaders: 1},
+                        },
+                        MediaQueryPlugin.loader,
+                        {
+                            loader: 'sass-loader',
+                        },
+                    ],
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg)$/i,
+                    loader: 'file-loader',
+                    options: {
+                        name: isDev ? '[name].[ext]' : '[name].[hash].[ext]',
+                        publicPath: './',
+                        esModule: false
+                    }
+                },
+                {
+                    test: /\.json$/,
+                    loader: 'json-loader'
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: jsLoaders()
+                },
+            ],
+        },
+        plugins: [
+            new CleanWebpackPlugin({}),
+            new MiniCssExtractPlugin({
+                filename: filename('css'),
+                chunkFilename: '[id].css',
+            }),
+            new ImageMinimizerPlugin({
+                minimizerOptions: {
+                    // Lossless optimization with custom option
+                    // Feel free to experiment with options for better result for you
+                    plugins: [
+                        ['gifsicle', { interlaced: true }],
+                        ['jpegtran', { progressive: true }],
+                        ['optipng', { optimizationLevel: 5 }],
+                        [
+                            'svgo',
+                            {
+                                plugins: [
+                                    {
+                                        removeViewBox: false,
+                                    },
+                                ],
+                            },
+                        ],
+                    ],
+                },
+            }),
+            new HtmlPlugin({
+                title: 'Hotel Rooms',
+                filename: 'index.html',
+                favicon: path.resolve(__dirname, 'src/favicon.ico'),
+                tags: {
+                    headTags: `
+                    <meta charset="UTF-8">
+                    `,
+                    bodyTags: `
+                    <div id="app"></div>
+                `
+                },
+                templateContent: ({htmlWebpackPlugin}) => {
+                    return `
+                    <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        ${htmlWebpackPlugin.options.tags.headTags}
+                        <title>${htmlWebpackPlugin.options.title}</title>
+                    </head>
+                    <body>
+                        ${htmlWebpackPlugin.options.tags.bodyTags}
+                    </body>
+                    </html>
+                  `
+                }
+            })
+        ]
+    }
+])
